427b8cb5253271c160db4ba8fbea7d143f2d7899
Heinrich Kruger <heindsight@kruger.dev>
2019-04-04 20:58:05 +0100

    Try better recognition

--- alexa.py
+++ alexa.py
@@ -3,6 +3,7 @@ import speech_recognition as sr
 
 def get_audio(recogniser):
     with sr.Microphone() as src:
+        recogniser.adjust_for_ambient_noise(src)
         print("Say something")
         return recogniser.listen(src)
 
@@ -11,6 +12,11 @@ def get_text():
     recogniser = sr.Recognizer()
 
     audio = get_audio(recogniser)
+
+    with open("sound.wav", "wb") as stream:
+        stream.write(audio.get_wav_data())
+
+    print("Analysing...")
 
     return recogniser.recognize_sphinx(audio)
 
